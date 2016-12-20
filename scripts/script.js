4d95799dea001519bc81c032df5ca02330bc144e
Tanya <tanya.murrow@hobbylobby.com>
2016-12-20 13:44:14 -0600

    Undo changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
   //$('#myModal').modal("show");
 });
+
+var currentPlayers = {};
+
+function AddPlayer(name)
+{
+  
+}
+
+function ScoreRound(bid, partner1, partner2)
+{
+  
+}
+
+function NewGame(player1, player2, player3, player4, player5)
+{
+  
+}
+
+function LoadGame()
+{
+  
+}
